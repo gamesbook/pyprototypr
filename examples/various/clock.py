e3c8633d06d2383979fac30f191f824b22783b3c
Derek Hohls <dhohls@creationbreeding.co.za>
2024-09-04 08:56:57 +0200

    Update examples

--- examples/various/clock.py
+++ examples/various/clock.py
@@ -20,7 +20,7 @@ Blueprint(stroke_width=0.5)
 Text(common=header, text="Basic Clock")
 
 # basic clock frame
-Circle(cx=3, cy=4.5, radius=2.5, fill=white, stroke_width=6,
+Circle(cx=3, cy=4.5, radius=2.5, stroke_width=6,
        label_size=6, label_dy=1, label="PROTO")
 # minutes
 Circle(cx=3, cy=4.5, radius=2.3, radii=steps(0,360,6), stroke=white, fill=None,
@@ -31,7 +31,7 @@ Circle(cx=3, cy=4.5, radius=2.3, radii=steps(0,360,30), stroke=white, fill=None,
 # centre
 Circle(cx=3, cy=4.5, radius=.13, fill=black)
 # hour hand
-Circle(cx=3, cy=4.5, radius=1.8, radii=[60], stroke=white, fill=None,
+Circle(cx=3, cy=4.5, radius=1.8, radii=[35], stroke=white, fill=None,
        radii_length=2, radii_offset=-.5,  radii_stroke_width=4)
 # minute hand
 Circle(cx=3, cy=4.5, radius=1.8, radii=[150], stroke=white, fill=None,
